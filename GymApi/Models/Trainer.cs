f3ebcf2217a7d1102a59ad4d59169e06070b67c8
Soria <rene.soria@ucb.edu.bo>
2025-09-26 00:12:58 -0400

    feat: creating class Trainer

--- GymApi/Models/Trainer.cs
+++ GymApi/Models/Trainer.cs
@@ -1,6 +1,15 @@
-﻿namespace GymApi.Models
-{
-    public class Trainer
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Trainer
     {
+    public Guid ID { get; set; }
+    [Required, StringLength(100)]
+    public string FullName { get; set; }= string.Empty;
+    [Required, StringLength(10)]
+    public string Speciality {  get; set; }= string.Empty;
+    [Required]
+    public bool Certified { get; set; }
+
     }
-}
+
