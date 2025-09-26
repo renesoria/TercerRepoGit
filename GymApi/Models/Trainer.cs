f9aa95ccc2c9a5d272abc6128937dc984e6dcebd
Soria <rene.soria@ucb.edu.bo>
2025-09-26 00:32:21 -0400

    fix: error from declaration variable 'specialty'

--- GymApi/Models/Trainer.cs
+++ GymApi/Models/Trainer.cs
@@ -7,9 +7,9 @@ public class Trainer
     [Required, StringLength(100)]
     public string FullName { get; set; }= string.Empty;
     [Required, StringLength(10)]
-    public string Speciality {  get; set; }= string.Empty;
+    public string Specialty {  get; set; }= string.Empty;
     [Required]
-    public bool Certified { get; set; }
+    public bool Certified { get; set; }=false;
 
     }
 
@@ -18,7 +18,7 @@ public record CreateTrainerDto
     [Required, StringLength(100)]
     public string FullName { get; init; } = string.Empty;
     [Required, StringLength(10)]
-    public string Speciality { get; init; } = string.Empty;
+    public string Specialty { get; init; } = string.Empty;
 }
 
 public record UpdateTrainerDto
@@ -26,5 +26,5 @@ public record UpdateTrainerDto
     [Required, StringLength(100)]
     public string FullName { get; init; } = string.Empty;
     [Required, StringLength(10)]
-    public string Speciality { get; init; } = string.Empty;
+    public string Specialty { get; init; } = string.Empty;
 }
