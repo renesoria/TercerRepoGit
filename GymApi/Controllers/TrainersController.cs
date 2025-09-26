4f6a48f30d3fdd44957d2e8e4099327146cdd90b
Soria <rene.soria@ucb.edu.bo>
2025-09-26 01:02:36 -0400

    feat: add NormalizePage and Order

--- GymApi/Controllers/TrainersController.cs
+++ GymApi/Controllers/TrainersController.cs
@@ -1,6 +1,34 @@
-﻿namespace GymApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+namespace GymApi.Controllers
 {
-    public class TrainersController
+    [ApiController]
+    [Route("api/v1/[controller")]
+    public class TrainersController: ControllerBase
     {
+        private static readonly List<Trainer> _trainers = new()
+        {
+            new Trainer{Id= Guid.NewGuid(), FullName="Dante Soria", Specialty="cardio", Certified=true},
+            new Trainer{Id= Guid.NewGuid(), FullName="David Andazola", Specialty="funcional", Certified=true},
+            new Trainer{Id= Guid.NewGuid(), FullName="Denar Yamil", Specialty="cardio", Certified=true},
+            new Trainer{Id= Guid.NewGuid(), FullName="Denis Azpilicueta", Specialty="yoga", Certified=false},
+            new Trainer{Id= Guid.NewGuid(), FullName="Giovani Gandarillas", Specialty="yoga", Certified=false},
+        };
+        private static (int page, int limit) NormalizePage(int? page, int ? limit)
+        {
+            var p = page.GetValueOrDefault(1); if (p < 1) p = 1;
+            var l = limit.GetValueOrDefault(10); if (l < 1) l = 1;if(l> 100) l = 100;
+            return (p, l);
+        }
+        private static IEnumerable<T> OrderByProp<T>(IEnumerable<T> src, string? sort, string? order)
+        {
+            if (string.IsNullOrWhiteSpace(sort)) return src;
+            var prop = typeof(T).GetProperty(sort, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (prop is null) return src;
+
+            return string.Equals(order, "desc", StringComparison.OrdinalIgnoreCase)
+                ? src.OrderByDescending(x => prop.GetValue(x))
+                : src.OrderBy(x => prop.GetValue(x));
+        }
     }
 }
